2f945d35e8d4ee6c8b29466caedf268f0c4ff5c5
sideeqhammed <sideeqhammed05.com>
2025-10-16 15:28:18 +0100

    Styled Header.jsx

--- Book_Library_FE/src/components/Header.jsx
+++ Book_Library_FE/src/components/Header.jsx
@@ -2,12 +2,13 @@ import { Link } from "react-router-dom"
 
 function Header() {
   return (
-    <div className="flex flex-wrap md:flex-nowrap text-black justify-between py-5 px-15 text-center items-center">
-      <h1><Link to={'/'}>Book LiBrary</Link></h1>
+    <div className="mx-auto py-4 px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+      <h1 className="
+          text-amber-600 flex items-center transition-colors hover:text-amber-700"><Link to={'/'}>Book LiBrary</Link></h1>
       <ul className="flex ">
         {/* <li className="mr-5 text-blue-500"><Link to={'/'}>Search</Link></li> */}
         {/* <li className="mr-5"><a>Favourites</a></li> */}
-        <li className="mr-5"><a>Contact Us</a></li>
+        <li className="mr-5"><a className="text-base text-gray-700 hover:text-amber-500 transition-colors duration-200 cursor-pointer ">Contact Us</a></li>
       </ul>
     </div>
   )
